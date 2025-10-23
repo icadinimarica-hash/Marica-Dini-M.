6a9b71ce1d493c29cfaa3446437144f41618cafc
icadinimarica-hash <icadinimarica@gmail.com>
2025-10-23 10:11:58 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+conts tombolSapa = document. getElementById(sapabutton');
+// 2. Tambahan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click', function() {
+  // 3. Jelaskan kode ini ketika tombol di-klik
+  alert('Halo! Terima kasih sudah berkunjung! ');
+});
